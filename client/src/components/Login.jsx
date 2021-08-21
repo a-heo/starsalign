e4765d244e90cb4c02340a3f874ef60c7f6cbb90
Annie Heo <annie.heo@gmail.com>
2021-08-20 22:49:37 -0700

    set up login form for users to login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [userId, updateID] = useState('');
+  const [password, updatePW] = useState('');
+
+  const handleSubmit = () => {
+    console.log('submit Pressed');
+  };
+
   return (
-      <div>login section</div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          User Id:
+          <input type="text" value={userId} onChange={(e) => updateID(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => updatePW(e.target.value)} />
+        </label>
+      </form>
+    </div>
   );
 };
 
