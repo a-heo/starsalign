75c60271ff38a81d3ad0dc33bbe0973daf649aa6
Annie Heo <annie.heo@gmail.com>
2021-11-10 15:21:59 -0800

    create journal form

--- client/src/components/JournalForm.jsx
+++ client/src/components/JournalForm.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const JournalForm = () => {
+  const [title, setTitle] = useState('');
+  const [entry, setEntry] = useState('');
+
   const handleSubmit = (e) => {
     console.log(e);
   };
+
   return (
     <form>
-      <label>Title</label>
-      <label>Entry</label>
+      <div>
+        <label>Title</label>
+        <br />
+        <textarea name="title" placeholder="Enter Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+      </div>
+      <div>
+        <label>Entry</label>
+        <br />
+        <textarea name="entry" placeholder="Write your thoughts" rows="5" value={entry} onChange={(e) => setEntry(e.target.value)} required />
+      </div>
     </form>
   );
 };
