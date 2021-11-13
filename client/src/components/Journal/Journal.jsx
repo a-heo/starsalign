afe2b6521837887da671c93850ab595963fd583e
Annie Heo <annie.heo@gmail.com>
2021-11-12 17:26:08 -0800

    clean up code

--- client/src/components/Journal/Journal.jsx
+++ client/src/components/Journal/Journal.jsx
@@ -10,29 +10,29 @@ const Journal = () => {
   const { user, setUser } = useContext(UserContext);
 
   const today = new Date().toLocaleDateString();
-  
+
   const getEntries = (id) => {
     axios.get(`/user/${id}/journal`)
-    .then((result) => {
-      //probably better storing all info into user so logout is cleaner?
-      setEntries(result.data);
-    })
-    .catch((error) => {
-      console.log(error, 'error in retrieving journal entries');
-    });
-  } 
+      .then((result) => {
+      // probably better storing all info into user so logout is cleaner?
+        setEntries(result.data);
+      })
+      .catch((error) => {
+        console.log(error, 'error in retrieving journal entries');
+      });
+  };
 
   useEffect(() => {
     getEntries(user.id);
   }, []);
 
-  //refactor code to delete entry from db and load new entries
   const deleteEntry = (journalId) => {
     axios.delete(`/user/${journalId}/journal`)
-      .then(result => {
+      .then((result) => {
         getEntries(user.id);
       })
-      .catch(error => {
+      .catch((error) => {
+        console.log(error, 'error in deleteentry');
       });
   };
 
