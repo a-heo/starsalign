630129024d9a5d62ef0e4eb62a60d5c2e2958354
Annie Heo <annie.heo@gmail.com>
2022-01-24 13:35:40 -0800

    moved form html into the modal body

--- client/src/components/Journal/JournalForm.jsx
+++ client/src/components/Journal/JournalForm.jsx
@@ -47,41 +47,41 @@ const JournalForm = ({ setEntries, setModal, modalOn }) => {
                 {today}
               </h3>
             </ModalHeader>
-          </ModalContent>
           <ModalBody>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Title</label>
+        <br />
+        <textarea name="title" placeholder="Enter Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+      </div>
+      <div>
+        <label>Entry</label>
+        <br />
+        <textarea name="entry" placeholder="Write your thoughts" rows="5" value={entry} onChange={(e) => setEntry(e.target.value)} required />
+      </div>
+      <div>
+        <label>Emotional Level of this Entry</label>
+        <br />
+        <select value={emotion} onChange={(e) => setEmotion(e.target.value)} required>
+          <option />
+          <option value="happy">Happy</option>
+          <option value="neutral">Neutral</option>
+          <option value="difficult">Difficult</option>
+          <option value="dark">Deep Abyss</option>
+        </select>
+      </div>
+      <br />
+      <input type="submit" value="submit" />
+    </form>
             This is where the journal form appears
           </ModalBody>
           <ModalFooter>
             <button onClick={() => { setModal(!modalOn); }}>Close</button>
           </ModalFooter>
+          </ModalContent>
         </Modal>
       ) : null}
     </>
-    // <form onSubmit={handleSubmit}>
-    //   <div>
-    //     <label>Title</label>
-    //     <br />
-    //     <textarea name="title" placeholder="Enter Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
-    //   </div>
-    //   <div>
-    //     <label>Entry</label>
-    //     <br />
-    //     <textarea name="entry" placeholder="Write your thoughts" rows="5" value={entry} onChange={(e) => setEntry(e.target.value)} required />
-    //   </div>
-    //   <div>
-    //     <label>Emotional Level of this Entry</label>
-    //     <br />
-    //     <select value={emotion} onChange={(e) => setEmotion(e.target.value)} required>
-    //       <option />
-    //       <option value="happy">Happy</option>
-    //       <option value="neutral">Neutral</option>
-    //       <option value="difficult">Difficult</option>
-    //       <option value="dark">Deep Abyss</option>
-    //     </select>
-    //   </div>
-    //   <br />
-    //   <input type="submit" value="submit" />
-    // </form>
   );
 };
 
